d028f00cb6c549393daf64847eda36d22135f663
DanielVz <danielrvp27@gmail.com>
2023-06-16 23:56:23 -0700

    calendario configuraciones iniciales

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,52 @@
-import { Navbar } from "../"
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns';
+import { CalendarEvent, Navbar } from '../';
+
+import { localizer, getMessagesES } from '../../helpers';
+
+const events = [{
+  title: 'Cumpleaños del sascuash',
+  note: 'Hay que comprar las kwamas',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Daniel'
+  }
+}]
 
 export const CalendarPage = () => {
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+    return {
+      style
+    }
+  }
   return (
     <>
       <Navbar />
+
+      <Calendar
+      culture='es'
+      localizer={ localizer }
+      events={ events }
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc( 100vh - 80px )' }}
+      messages={ getMessagesES() }
+      eventPropGetter={ eventStyleGetter }
+      components={{
+        event: CalendarEvent
+      }}
+    />
     </>
   )
 }
